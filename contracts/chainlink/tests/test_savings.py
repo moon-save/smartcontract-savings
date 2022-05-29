8fb0888a1f4a21d11f888b27703fa484770d3249
Daniel Wang <danielw68@gmail.com>
2022-05-28 23:55:27 -0400

    finish

--- contracts/chainlink/tests/test_savings.py
+++ contracts/chainlink/tests/test_savings.py
@@ -14,36 +14,41 @@ def test_cannot_enter():
     savings = deploy_lottery()
 
     with pytest.raises(exceptions.VirtualMachineError):
-        savings.depositSavings(0, {"from": get_account(), "value": 10})
+        savings.depositSavings({"from": get_account(), "value": 10})
 
 
 def test_can_enter_savings():
     savings = deploy_lottery()
     savings.createLottery(1, 30, {"from": get_account()})
-    savings.depositSavings(0, {"from": get_account(), "value": 10})
+    savings.depositSavings({"from": get_account(), "value": 10})
 
     assert savings.getPlayersCount() == 1
 
 
 def test_can_pick_winner():
+    totalInterest = 12*2
+
     savings = deploy_lottery()
     savings.createLottery(1, 4, {"from": get_account()})
     
-    savings.depositSavings(0, {"from": get_account(), "value": 15})
-    savings.depositSavings(0, {"from": get_account(index=1), "value": 3})
-    savings.depositSavings(0, {"from": get_account(index=2), "value": 2})
+    savings.depositSavings({"from": get_account(), "value": 15})
+    savings.depositSavings({"from": get_account(index=1), "value": 3})
+    savings.depositSavings({"from": get_account(index=2), "value": 2})
 
-    savings.depositInterest({"from": get_account(index=3), "value": 20})
+    num_participants = savings.getPlayersCount()
+    assert num_participants == 3
+    
+    savings.depositInterest({"from": get_account(index=3), "value": totalInterest})
 
-    assert savings.getInterestPool({"from": get_account()}) == 20
+    assert savings.getInterestPool({"from": get_account()}) == totalInterest
 
-    savings_balance = savings.getInterestPool()
+    interest_balance = savings.getInterestPool()
     account_balance = get_account(index=0).balance()
 
     fund_with_link(savings)
     time.sleep(5)
 
-    transaction = savings.declareWinner(0, {"from": get_account()})
+    transaction = savings.declareWinner({"from": get_account()})
     request_id = transaction.events['RandomnessRequested']['requestId']
     STATIC_RNG = 10
     get_contract("vrf_coordinator").callBackWithRandomness(request_id,
@@ -55,4 +60,5 @@ def test_can_pick_winner():
     #     print(f"Winner: {winner}")
 
     assert savings.getWinners()[0] == get_account(index=0)
-    assert get_account(index=0).balance() == (savings_balance / 4 * 3) + account_balance
+    new_balance = int(interest_balance / 2 / 4 * 3) + account_balance + int(totalInterest/2/num_participants)
+    assert get_account(index=0).balance() == new_balance
